bae8f644a0d58935c12e31c6cd861a0703af761a
ViiRo <45905915+ViiRo@users.noreply.github.com>
2018-12-20 23:06:36 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("525422648717475857")
+setInterval(function() {
+channel.send(`@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]@everyone ks omkm ya nsaben :]]]]]`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
